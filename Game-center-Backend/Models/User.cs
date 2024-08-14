443a0e295cc94e8bed30c2dacec869e0e41d353e
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-08-14 19:31:55 +0330

    Create User Model

---
+++ Game-center-Backend/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Game_center_Backend.Models;
+
+[Table("Users")]
+public class User
+{
+    [Required]
+    [Key] 
+    [SwaggerIgnore] 
+    public Guid Id { get; set; } = new Guid();
+    
+    [Required(ErrorMessage = "Username is Required !!!")]
+    [Display(Name = "Username")]
+    [MaxLength(300)]
+    public string Username { get; set; }
+    
+    [Required(ErrorMessage = "Password is Required !!!")]
+    [Display(Name = "Password")]
+    [MaxLength(300)]
+    public string Password { get; set; }
+    
+    [Required(ErrorMessage = "Email is Required !!!")]
+    [Display(Name = "Email")]
+    [MaxLength(300)]
+    public string Email { get; set; }
+    
+    [Display(Name = "Games")]
+    [MaxLength(300)]
+    public string Games { get; set; }
+    
+    [Required]
+    [MaxLength(40)]
+    public string adminRole { get; set; }
+    
+}
