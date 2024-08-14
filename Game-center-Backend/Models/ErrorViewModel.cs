e68f0b102ff9c1ba3d67e8bb69eb2d29cf908620
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-08-14 19:25:19 +0330

    install Nuget Packages

--- Game-center-Backend/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Game_center_Backend.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
