59b87dbb6903a858c43b7dab678d703a9de7c8c5
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-08-14 19:54:37 +0330

    Create User Controller

---
+++ Game-center-Backend/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Game_center_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
