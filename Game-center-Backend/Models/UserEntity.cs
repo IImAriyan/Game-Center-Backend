f70d7571d5ca22e3f7808076739d1acd160766b6
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-08-20 22:02:46 +0330

    Change Models

--- Game-center-Backend/Models/UserEntity.cs
+++ Game-center-Backend/Models/UserEntity.cs
@@ -7,21 +7,18 @@ namespace Game_center_Backend.Models;
 [Table("Users")]
 public class UserEntity
 {
-    [Required]
-    [Key] 
-    [SwaggerIgnore] 
-    public Guid Id { get; set; } = new Guid();
-    
+    [Required] [Key] [SwaggerIgnore] public Guid Id { get; set; } = new Guid();
+
     [Required(ErrorMessage = "Username is Required !!!")]
     [Display(Name = "Username")]
     [MaxLength(300)]
     public string Username { get; set; }
-    
+
     [Required(ErrorMessage = "Password is Required !!!")]
     [Display(Name = "Password")]
     [MaxLength(300)]
     public string Password { get; set; }
-    
+
     [Required(ErrorMessage = "Email is Required !!!")]
     [Display(Name = "Email")]
     [MaxLength(300)]
@@ -34,4 +31,43 @@ public class UserEntity
     [MaxLength(40)]
     public string adminRole { get; set; }
 
+
+
+    [Display(Name = "firstName")]
+    [MaxLength(40)]
+    public string firstName { get; set; }
+    
+    [Display(Name = "lastName")]
+    [MaxLength(40)]
+    public string lastName { get; set; }
+    
+    
+    [Display(Name = "age")]
+    [MaxLength(100)]
+    public int age { get; set; }
+
+    [Display(Name = "telephone")]
+    [MaxLength(200)]
+    public string telephone { get; set; }
+    
+    [Display(Name = "nationalCode")]
+    [MaxLength(200)]
+    public string nationalCode { get; set; }
+    
+    [Display(Name = "gender")]
+    [MaxLength(200)]
+    public string gender { get; set; }
+
+    [Display(Name = "accountLocked")]
+    [MaxLength(200)]
+    public string accountLocked { get; set; }
+    
+    [Display(Name = "coins")]
+    [MaxLength(200)]
+    public string coins { get; set; }
+
+    [Display(Name = "accountBanned")]
+    [MaxLength(200)]
+    public string accountBanned { get; set; }
+    
 }
